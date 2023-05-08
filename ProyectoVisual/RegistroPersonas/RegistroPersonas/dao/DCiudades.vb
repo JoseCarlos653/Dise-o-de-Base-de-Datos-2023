3e3852e4ab45200df85e5dcbec352982bc2b9df5
jose.perez14734@est.uca.edu.ni <jose.perez14734@est.uca.edu.ni>
2023-05-08 16:05:31 -0600

    Se agrego la conexion con la base de datos y llenarRegistros

---
+++ ProyectoVisual/RegistroPersonas/RegistroPersonas/dao/DCiudades.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Data.SqlClient
+Public Class DCiudades
+
+    Public Function MostrarRegistros() As DataSet
+        Dim ds As New DataSet
+        Try
+            Dim conn As New SqlConnection(strConexion)
+            Dim tsql As String = "Select id as N'CODIGO', nombre as N'CIUDAD' from Ciudad"
+            Dim da As New SqlDataAdapter(tsql, conn)
+            da.Fill(ds)
+        Catch ex As Exception
+            MsgBox("Ocurrió un error al obtener los registros" & ex.Message, MsgBoxStyle.Critical, "Error")
+        End Try
+
+        Return ds
+    End Function
+
+End Class
--- ProyectoVisual/RegistroPersonas/RegistroPersonas/formularios/FrmCiudad.vb
+++ ProyectoVisual/RegistroPersonas/RegistroPersonas/formularios/FrmCiudad.vb
@@ -1,3 +1,15 @@
 ﻿Public Class FrmCiudad
 
+    Sub LlenarRegistros()
+        Dim idCiudad As New DCiudades
+        DgvRegistros.DataSource = idCiudad.MostrarRegistros.Tables(0)
+        DgvRegistros.Refresh()
+        GrbRegistros.Text = "Registros almacenados: " & DgvRegistros.Rows.Count
+
+    End Sub
+
+    Private Sub FrmCiudad_Load(sender As Object, e As EventArgs) Handles Me.Load
+        LlenarRegistros()
+    End Sub
+
 End Class
